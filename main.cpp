b6ddd2fda2853e731b41f5d0059292e8a1e70635
NINA <ninatsao@ningningning.lan>
2024-09-29 05:51:30 -0700

    2nd commit

--- main.cpp
+++ main.cpp
@@ -8,11 +8,34 @@ private:
     int blue;
 public:
     int getRed() const {return red;}
+    void setRed(int r) {red = r;}
+
     int getGreen() const {return green;}
+    void setGreen(int g) {green = g;}
+
     int getBlue() const {return blue;}
+    void setBlue(int b) {blue = b;}
+
+    void print() {
+        cout << "Color:" << endl;
+        cout << "\tRed: " << red << endl;
+        cout << "\tGreen: " << green << endl;
+        cout << "\tBlue: " << blue << endl;
+    }
 };
 
 int main() {
+    Color c1;
+    c1.setRed(255);
+    c1.setGreen(0);
+    c1.setBlue(0);
+    c1.print();
+    
+    Color c2;
+    c2.setRed(0);
+    c2.setGreen(255);
+    c2.setBlue(0);
+    c2.print();
 
     return 0;
 }
