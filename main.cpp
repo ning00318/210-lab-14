6fe0809f21debb2c36e046d56ad77e029fbef80b
NINA <ninatsao@ningningning.lan>
2024-09-29 05:16:24 -0700

    210-lab-14

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+public:
+    int getRed() const {return red;}
+    int getGreen() const {return green;}
+    int getBlue() const {return blue;}
+};
+
+int main() {
+
+    return 0;
+}
